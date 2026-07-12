e3bca1140503eb44f230c5bc38e7fc1b11e45fb0
Vanshika Varshney <varshney23queen11@gmail.com>
2026-07-12 23:07:30 +0530

    Add files via upload

---
+++ booleanExpression.cpp
@@ -0,0 +1,20 @@
+// code of boolean expression
+
+#include <iostream>
+using namespace std;
+ 
+ int main (){
+     int a = 100;      // local variable declaration
+     int k = 800;
+ 
+  // checking boolean condition
+  if ( a == 100 ){       // if condition is true then check following
+      if ( k == 800 ){     // if condition is true then print the  following
+          cout<<" value of a is 100 and value of k is 800 "<<endl;
+      }
+  }
+ cout<<"exact value of a is : "<<a<<endl;
+ cout<<"exact value of k is : "<<k<<endl;
+ 
+ return 0;
+ }
