dca94965164d7046a300e1d25337ff9097d7e044
Vanshika Varshney <varshney23queen11@gmail.com>
2026-07-19 07:40:38 +0530

    Add files via upload

---
+++ protected access modifier.cpp
@@ -0,0 +1,23 @@
+// PROTECTED ACCESS MODIFIER IN C++ 
+
+#include <iostream>
+using namespace std;
+
+class base{
+    protected :
+     int a;
+    private:
+     int b;
+};
+
+class derived :  protected base{
+   
+};
+
+int main(){
+    base b;
+    derived d;
+   // cout<<d.a;  will not work since 'a' is protected in both base as well as derived class
+    
+    return 0;
+}
