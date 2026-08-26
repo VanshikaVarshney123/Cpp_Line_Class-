e1d6e2fe6c918a83b4df682bea0c5a4a29a3255c
Vanshika Varshney <varshney23queen11@gmail.com>
2026-08-26 15:55:13 +0530

    Add files via upload

---
+++ codechef1.cpp
@@ -0,0 +1,10 @@
+  // Write a program to output the squares (using multiplication) of numbers from 1 to 5 on separate lines.
+#include <iostream>
+using namespace std;
+
+int main() {
+    for( int n = 1; n<=5; n++ ){
+	cout<< n <<"-"<< n*n <<endl;
+	 }
+    return 0;
+}
