3d276e28117d06646c7fc3b9e383bfdb8598fb55
Vanshika Varshney <varshney23queen11@gmail.com>
2026-08-27 20:10:16 +0530

    Add files via upload

---
+++ merging two arrays.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr1[100] = {2, 4, 6}; // First array
+    int size1 = 3;
+
+    int arr2[100] = {8, 10, 12, 14}; // Second array
+    int size2 = 4;
+
+    // Solution as follows
+    int merged[200]; // Merged array
+    int mergedSize = size1 + size2; // Size of the merged array
+
+    // Copy elements from the first array to the merged array
+    for(int i = 0; i < size1; i++) {
+        merged[i] = arr1[i];
+    }
+
+    // Copy elements from the second array to the merged array
+    for(int i = 0; i < size2; i++) {
+        merged[size1 + i] = arr2[i];
+    }
+    
+    // Print the merged array
+    for(int i = 0; i < mergedSize; i++){
+        cout << merged[i] << " ";
+    }
+
+    return 0;
+}
