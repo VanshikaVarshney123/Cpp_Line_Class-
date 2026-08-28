729097c9a02eebb0e959d765d94d20975ebc1219
Vanshika Varshney <varshney23queen11@gmail.com>
2026-08-28 16:01:14 +0530

    Add files via upload

---
+++ aRROW OPERATOR.cpp
@@ -0,0 +1,30 @@
+// ARROW OPERATOR IN C++
+
+#include <iostream>
+using namespace std;
+
+class complex{
+    int real, imaginary;
+    public:
+     void getData() {
+        cout<<"The real part is "<<real<<endl;
+        cout<<"The imaginary part is "<<imaginary<<endl;
+     }
+     void setData(int a, int b){
+        real = a;
+        imaginary = b;
+     }
+};
+
+int main() {
+    // complex c1;
+    // complex *ptr = &c1;
+    complex *ptr = new complex;
+   // (*ptr).setData(6,8);  is same as ---
+   ptr -> setData(6,8);
+   // (*ptr).getData();  is good as ----
+   ptr -> getData();
+
+
+    return 0;
+} 
